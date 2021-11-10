4e036264d487a07035dbaa752c57722dc90f0f82
imchicano <j.castrejonvillarreal@outlook.com>
2021-11-10 13:20:07 -0600

    gifGrid updated

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -4,14 +4,15 @@ import { GifGridItem } from './GifGridItem';
 
 export const GifGrid = ( {category} ) => {
 
-    const {data:images, loading} = useFetchGifs( category );
+    const {data:images} = useFetchGifs( category );
 
     return (
         <>
             <h3>{category}</h3>
-            {loading && <p>Cargando...</p>}
+            
             <div className="card-grid">
                 {
+                    
                     images.map( img => (
                             <GifGridItem 
                                 key={img.id}
